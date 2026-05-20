c83df3cee232a62cf40480442b691bc43520ce0a
Skip <skip@usecurling.com>
2026-05-20 16:30:26 +0000

    Refactor UI to use database for translations

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,3 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { Toaster } from '@/components/ui/toaster'
-import { Toaster as Sonner } from '@/components/ui/sonner'
-import { TooltipProvider } from '@/components/ui/tooltip'
 import { lazy, Suspense, useEffect } from 'react'
 import {
   BrowserRouter,
